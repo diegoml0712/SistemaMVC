c1d54e53e2ac066f2f49024c0abb0a1f7669b175
Diego Pereira <diego-l2@hotmail.com.br>
2020-10-03 06:44:36 -0300

    Criado o botão excluir e adicionado imagem agenda

---
+++ SistemaMVC/View/CadTelefone.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace View
+{
+    partial class CadTelefone
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CadTelefone));
+            this.lblBusca = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblFone = new System.Windows.Forms.Label();
+            this.txtBusca = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.txtFone = new System.Windows.Forms.MaskedTextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBusca
+            // 
+            this.lblBusca.AutoSize = true;
+            this.lblBusca.Location = new System.Drawing.Point(20, 58);
+            this.lblBusca.Name = "lblBusca";
+            this.lblBusca.Size = new System.Drawing.Size(40, 13);
+            this.lblBusca.TabIndex = 0;
+            this.lblBusca.Text = "Busca:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(20, 106);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblFone
+            // 
+            this.lblFone.AutoSize = true;
+            this.lblFone.Location = new System.Drawing.Point(20, 132);
+            this.lblFone.Name = "lblFone";
+            this.lblFone.Size = new System.Drawing.Size(52, 13);
+            this.lblFone.TabIndex = 0;
+            this.lblFone.Text = "Telefone:";
+            // 
+            // txtBusca
+            // 
+            this.txtBusca.Location = new System.Drawing.Point(81, 51);
+            this.txtBusca.Name = "txtBusca";
+            this.txtBusca.Size = new System.Drawing.Size(181, 20);
+            this.txtBusca.TabIndex = 1;
+            this.txtBusca.TextChanged += new System.EventHandler(this.txtBusca_TextChanged);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(81, 103);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(181, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(285, 48);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 2;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(285, 75);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nome,
+            this.Telefone});
+            this.grid.Location = new System.Drawing.Point(17, 164);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid.Size = new System.Drawing.Size(343, 180);
+            this.grid.TabIndex = 3;
+            this.grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_click);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "Id";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 50;
+            // 
+            // Nome
+            // 
+            this.Nome.DataPropertyName = "Nome";
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
+            this.Nome.Width = 150;
+            // 
+            // Telefone
+            // 
+            this.Telefone.DataPropertyName = "Telefone";
+            this.Telefone.HeaderText = "Telefone";
+            this.Telefone.Name = "Telefone";
+            this.Telefone.ReadOnly = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(285, 103);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 4;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // txtFone
+            // 
+            this.txtFone.Location = new System.Drawing.Point(81, 129);
+            this.txtFone.Mask = "(00)99999-9999";
+            this.txtFone.Name = "txtFone";
+            this.txtFone.Size = new System.Drawing.Size(99, 20);
+            this.txtFone.TabIndex = 5;
+            this.txtFone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtFone.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(20, 80);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(81, 77);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(181, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CadTelefone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 373);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtFone);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtBusca);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblFone);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblBusca);
+            this.Name = "CadTelefone";
+            this.Text = "Agenda Telefonica";
+            this.Load += new System.EventHandler(this.CadTelefone_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBusca;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblFone;
+        private System.Windows.Forms.TextBox txtBusca;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.MaskedTextBox txtFone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SistemaMVC/View/CadTelefone.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using View.Entidade;
+using View.Models;
+
+namespace View
+{
+    public partial class CadTelefone : Form
+    {
+        public CadTelefone()
+        {
+            InitializeComponent();
+        }
+
+        UsuarioEnt objTabela = new UsuarioEnt();
+
+        private void LimparCampos()
+        {
+            txtID.Clear();
+            txtNome.Clear();
+            txtFone.Clear();
+            txtNome.Focus();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            objTabela.Nome = txtNome.Text;
+            objTabela.Telefone = txtFone.Text;
+
+            int x = UsuarioModelo.Inserir(objTabela);
+
+            if (x > 0)
+            {
+                MessageBox.Show(string.Format("Usuario {0} foi inserido!", txtNome.Text));
+                ListaGrid();
+            }
+            else
+            {
+                MessageBox.Show("Não inserido!");
+            }
+        }
+
+        private void ListaGrid()
+        {
+            try
+            {
+                List<UsuarioEnt> lista = new List<UsuarioEnt>();
+                lista = new UsuarioModelo().Lista();
+                grid.AutoGenerateColumns = false;
+                grid.DataSource = lista;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void CadTelefone_Load(object sender, EventArgs e)
+        {
+            ListaGrid();
+        }
+
+        private void grid_click(object sender, DataGridViewCellEventArgs e)
+        {
+            txtID.Text = grid.CurrentRow.Cells[0].Value.ToString();
+            txtNome.Text = grid.CurrentRow.Cells[1].Value.ToString();
+            txtFone.Text = grid.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void txtBusca_TextChanged(object sender, EventArgs e)
+        {
+            if (txtBusca.Text == "")
+            {
+                ListaGrid();
+                return;
+            }
+            try
+            {
+                LimparCampos();
+                txtBusca.Focus();
+                objTabela.Nome = txtBusca.Text;
+                List<UsuarioEnt> lista = new List<UsuarioEnt>();
+                lista = new UsuarioModelo().Consulta(objTabela);
+
+                grid.AutoGenerateColumns = false;
+                grid.DataSource = lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Erro ao listar dados" + ex.Message);
+            }
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objTabela.Id = Convert.ToInt32(txtID.Text);
+                objTabela.Nome = Convert.ToString(txtNome.Text);
+                objTabela.Telefone = Convert.ToString(txtFone.Text);
+
+                int x = UsuarioModelo.Editar(objTabela);
+                if (x > 0)
+                {
+                    MessageBox.Show(string.Format("Usuario {0} foi Editado!", txtNome.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Não foi alterado!");
+                }
+                LimparCampos();
+                ListaGrid();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objTabela.Id = Convert.ToInt32(txtID.Text);
+                objTabela.Nome = Convert.ToString(txtNome.Text);
+                objTabela.Telefone = Convert.ToString(txtFone.Text);
+
+                int x = UsuarioModelo.Excluir(objTabela);
+                if (x > 0)
+                {
+                    MessageBox.Show("Usuario Excluido!");
+                }
+                else
+                {
+                    MessageBox.Show("Não Excluido!");
+                }
+                LimparCampos();
+                ListaGrid();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
